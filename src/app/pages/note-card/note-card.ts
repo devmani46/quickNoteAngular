c7ff9836d634be3d89a0785f443c48465c7e4b43
devmani <devmani1456@gmail.com>
2025-08-15 16:36:12 +0545

    search fixed

--- src/app/pages/note-card/note-card.ts
+++ src/app/pages/note-card/note-card.ts
@@ -1,26 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-note-card',
   standalone: true,
-  imports: [CommonModule ],
+  imports: [CommonModule],
   templateUrl: './note-card.html',
   styleUrl: './note-card.css'
 })
 export class NoteCard implements OnInit {
+
+  @Input() searchText: string = '';
   notes: any[] = [];
+  filteredNotes: any[] = [];
 
   ngOnInit() {
+    this.loadNotes();
+  }
+
+  ngOnChanges() {
+    this.applyFilter();
+  }
+
+  loadNotes() {
     const storedNotes = localStorage.getItem('notes');
-    if (storedNotes) {
-      this.notes = JSON.parse(storedNotes);
-    }
+    this.notes = storedNotes ? JSON.parse(storedNotes) : [];
+    this.applyFilter();
+  }
+
+  applyFilter() {
+    const search = this.searchText.toLowerCase();
+    this.filteredNotes = this.notes.filter(note =>
+      note.title.toLowerCase().includes(search) ||
+      note.description.toLowerCase().includes(search)
+    );
   }
 
   deleteNote(index: number) {
-    this.notes.splice(index, 1);
+    const actualIndex = this.notes.indexOf(this.filteredNotes[index]);
+    this.notes.splice(actualIndex, 1);
     localStorage.setItem('notes', JSON.stringify(this.notes));
+    this.applyFilter();
   }
-
 }
--- src/app/pages/note-list/note-list.ts
+++ src/app/pages/note-list/note-list.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { NoteCard } from "../note-card/note-card";
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-note-list',
   standalone: true,
-  imports: [NoteCard, RouterLink],
+  imports: [NoteCard, RouterLink, FormsModule],
   templateUrl: './note-list.html',
   styleUrl: './note-list.css'
 })
 export class NoteList {
-searchText: any;
+  searchText: string = '';
 
+  filterNotes(event: Event) {
+    const value = (event.target as HTMLSelectElement).value;
+    console.log("Filter selected:", value);
+  }
 }
